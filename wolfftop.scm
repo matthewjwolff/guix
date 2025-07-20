8e8871287769463851c641022777b2a2b9299ac8
Matthew Wolff <matthewjwolff@gmail.com>
2025-07-19 19:38:07 -0500

    migrate rpi3 to wolfftop

--- wolfftop.scm
+++ wolfftop.scm
@@ -6,7 +6,7 @@
              (wolff channels)
              (wolff packages)
              (wolff services))
-(use-service-modules desktop sddm xorg ssh networking shepherd virtualization docker admin containers)
+(use-service-modules desktop sddm xorg ssh networking shepherd virtualization docker admin containers web certbot)
 (use-package-modules gnome ssh admin fonts java)
 (use-package-modules qt xorg tmux linux package-management)
 
@@ -75,10 +75,86 @@
                         ("mk64-hd" . "https://github.com/ghostlydark/mk64-hd")
                         ("hbc" . "https://github.com/fail0verflow/hbc")
                         ("libogc" . "https://github.com/devkitPro/libogc")
+                        ("smashremix" . "https://github.com/JSsixtyfour/smashremix")
                         ("mario-eclipse" . "https://github.com/JoshuaMKW/Super-Mario-Eclipse")))
-
+                         (service nginx-service-type
+                                  (nginx-configuration
+                                   (server-blocks (list
+                                                   #|(nginx-server-configuration
+                                                    (server-name (list "minecraft.wolff.io"))
+                                                    (listen '("443 ssl"))
+                                                    (root "")
+                                                    (index '())
+                                                    (raw-content (list "add_header Strict-Transport-Security \"max-age=604800\" always;"))
+                                                    (ssl-certificate "/etc/acme.sh/*.wolff.io_ecc/*.wolff.io.cer")
+                                                    (ssl-certificate-key "/etc/acme.sh/*.wolff.io_ecc/*.wolff.io.key")
+                                                    (locations (list
+                                                                (nginx-location-configuration
+                                                                 (uri "= /502.html")
+                                                                 (body (list "root /srv/web;")))
+                                                                (nginx-location-configuration
+                                                                 (uri "/")
+                                                                 (body (list "proxy_pass http://localhost:8100/ ;"
+                                                                             "proxy_set_header X-Forwarded-For $remote_addr ;"
+                                                                             "error_page 502 /502.html ;"))))))|#
+                                                   (nginx-server-configuration
+                                                    (server-name (list "jellyfin.wolff.io"))
+                                                    (listen '("443 ssl http2"))
+                                                    (root "")
+                                                    (index '())
+                                                    (ssl-certificate "/etc/certs/wolff.io/fullchain.pem")
+                                                    (ssl-certificate-key "/etc/certs/wolff.io/privkey.pem")
+                                                    (locations (list
+                                                                (nginx-location-configuration
+                                                                 (uri "/")
+                                                                 (body (list "proxy_pass http://localhost:8096;"
+                                                                             "proxy_pass_request_headers on;"
+                                                                             "proxy_set_header Host $host;"
+                                                                             "proxy_set_header X-Real-IP $remote_addr;"
+                                                                             "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;"
+                                                                             "proxy_set_header X-Forwarded-Proto $scheme;"
+                                                                             "proxy_set_header X-Forwarded-Host $http_host;"
+                                                                             "proxy_set_header Upgrade $http_upgrade;"
+                                                                             "proxy_set_header Connection $http_connection;"
+                                                                             "proxy_buffering off;"))))))
+                                                   (nginx-server-configuration
+                                                    (server-name (list "files.wolff.io"))
+                                                    (listen '("443 ssl"))
+                                                    ;; don't know what these two do, but it wasnt in the original config
+                                                    ;; checking with web.scm, these are the sentinel values for each that disable the config line
+                                                    (root "")
+                                                    (index '())
+                                                    (ssl-certificate "/etc/certs/wolff.io/fullchain.pem")
+                                                    (ssl-certificate-key "/etc/certs/wolff.io/privkey.pem")
+                                                    (raw-content (list "add_header Strict-Transport-Security \"max-age=604800\";"))
+                                                    (locations (list
+                                                                (nginx-location-configuration
+                                                                 (uri "/")
+                                                                 (body (list "root /srv/web;"
+                                                                             "autoindex on;")))
+                                                                (nginx-location-configuration
+                                                                 (uri "/private")
+                                                                 (body (list "alias /srv/web_private/;"
+                                                                             "autoindex on;"
+                                                                             "auth_basic \"Secure\";"
+                                                                             ;; TODO htpasswd hidden dependency (but also a secret)
+                                                                             "auth_basic_user_file /srv/htpasswd;"))))))))))
 
              (service containerd-service-type)
+
+                          (service certbot-service-type
+                                   (certbot-configuration
+                                    (webroot "/var/www") ;; needs a non-#f webroot because of unconditional mkdir-p
+                                    (default-location #f)
+                                    (certificates (list (certificate-configuration
+                                                         (name "wolff.io") ;; provide a name to strip wildcard from paths
+                                                         (domains '("*.wolff.io"))
+                                                         (challenge "dns-01")
+                                                         ;; note: hidden dependency on /etc/cloudflare_dns.sh TODO managed by sops
+                                                         (authentication-hook (file-append certbot-cloudflare-hook "/cloudflare-update-dns.sh"))
+                                                         (cleanup-hook (file-append certbot-cloudflare-hook "/cloudflare-clean-dns.sh")))))))
+
+
 
 
              (service oci-container-service-type
